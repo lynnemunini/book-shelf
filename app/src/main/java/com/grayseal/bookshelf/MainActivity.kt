d4dcab909a863b49f7e989e4dd30058cc2e03b93
lynnemunini <muninilynne65@gmail.com>
2023-01-13 16:47:59 +0300

    Test adding users to database

--- app/src/main/java/com/grayseal/bookshelf/MainActivity.kt
+++ app/src/main/java/com/grayseal/bookshelf/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.grayseal.bookshelf
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,6 +11,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.firestore.FirebaseFirestore
 import com.grayseal.bookshelf.ui.theme.BookShelfTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +19,23 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BookShelfTheme {
+                val db = FirebaseFirestore.getInstance()
+                val user: MutableMap<String, Any> = HashMap()
+                user["firstName"] = "Gray"
+                user["lastName"] = "Seal"
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    db.collection("users")
+                        .add(user)
+                        .addOnSuccessListener {
+                            Log.d("DB", "onCreate: ${it.id}")
+                        }.addOnFailureListener{
+                            Log.d("DB", "onFail: $it")
+                        }
+                    Greeting("Welcome to your Book Shelf")
                 }
             }
         }
