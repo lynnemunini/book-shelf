69e07267dfeef3aa8ff5d1e03d3437ee37d5baf7
lynnemunini <muninilynne65@gmail.com>
2023-01-13 23:28:00 +0300

    Setting up dependency injection

---
+++ app/src/main/java/com/grayseal/bookshelf/BookShelf Application.kt
@@ -0,0 +1,8 @@
+package com.grayseal.bookshelf
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BookShelfApplication: Application() {
+}
--- app/src/main/java/com/grayseal/bookshelf/MainActivity.kt
+++ app/src/main/java/com/grayseal/bookshelf/MainActivity.kt
@@ -13,7 +13,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.firebase.firestore.FirebaseFirestore
 import com.grayseal.bookshelf.ui.theme.BookShelfTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
