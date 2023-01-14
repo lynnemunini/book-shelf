f4b2bb2d151963fa16c387d5a01a170040c888d2
lynnemunini <muninilynne65@gmail.com>
2023-01-14 16:24:16 +0300

    Setting up app routing

--- app/src/main/java/com/grayseal/bookshelf/navigation/BookShelfScreens.kt
+++ app/src/main/java/com/grayseal/bookshelf/navigation/BookShelfScreens.kt
@@ -1,4 +1,25 @@
 package com.grayseal.bookshelf.navigation
 
 enum class BookShelfScreens {
+    SplashScreen,
+    LoginScreen,
+    CreateAccountScreen,
+    HomeScreen,
+    SearchScreen,
+    ShelfScreen,
+    BookScreen;
+
+    companion object {
+        fun fromRoute(route: String): BookShelfScreens
+        = when(route.substringBefore("/")){
+            SplashScreen.name -> SplashScreen
+            LoginScreen.name -> LoginScreen
+            CreateAccountScreen.name -> CreateAccountScreen
+            SearchScreen.name -> SearchScreen
+            HomeScreen.name -> HomeScreen
+            ShelfScreen.name -> ShelfScreen
+            BookScreen.name -> BookScreen
+            else -> throw IllegalArgumentException("Route $route is not recognized")
+        }
+    }
 }
--- app/src/main/java/com/grayseal/bookshelf/screens/SplashScreen.kt
+++ app/src/main/java/com/grayseal/bookshelf/screens/SplashScreen.kt
@@ -1,2 +1,7 @@
 package com.grayseal.bookshelf.screens
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SplashScreen(){
+}
