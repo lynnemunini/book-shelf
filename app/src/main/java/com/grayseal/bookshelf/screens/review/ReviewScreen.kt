365e2e40b89a016f77d5e3954742843e850b04a0
lynnemunini <muninilynne65@gmail.com>
2023-03-04 11:07:40 +0300

    Review Screen

--- app/src/main/java/com/grayseal/bookshelf/screens/review/ReviewScreen.kt
+++ app/src/main/java/com/grayseal/bookshelf/screens/review/ReviewScreen.kt
@@ -1,11 +1,64 @@
 package com.grayseal.bookshelf.screens.review
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.grayseal.bookshelf.components.NavBar
+import com.grayseal.bookshelf.ui.theme.poppinsFamily
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ReviewScreen(
     navController: NavController
 ) {
+    Scaffold(content = { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(20.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        "Reviews",
+                        fontFamily = poppinsFamily,
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.onBackground,
+                        textAlign = TextAlign.Center
+                    )
+                    Text(
+                        "Your Reviews: A curated collection of all the books you have reviewed or rated.",
+                        fontFamily = poppinsFamily,
+                        fontSize = 13.sp,
+                        color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.6f),
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+        }
+    },
+        bottomBar = {
+            NavBar(navController = navController)
+        })
+
 
 }
